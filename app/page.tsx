eb2ec61c4d351601d501f3a4ef8f8eb606904d5a
Seon-U <seonu.kim.kr@gmail.com>
2025-11-12 10:47:29 +0900

    root page summary

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      {/* iframe youtube 영상왼쪽 */}
+      {/* 오른쪽 아코디언 활용한 playlist-video영역 */}
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>
+            <div className="flex flex-col">
+              <h1 className="font-bold">Playlist.title</h1>
+              <p className="text-gray-600 text-sm">Playlist.descriptiton</p>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent>
+            {/* 왼쪽에 video thumbnail image */}
+            {/* 오른쪽에 video title, video descriptiton을 flex-col로 */}
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
 }
